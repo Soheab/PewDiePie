a1337c62d6d7fa259d801a90a031a5e8c788da3c
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-02-07 19:26:48 -0500

    Removed commands which are included in seperate cogs now

--- cogs/general.py
+++ cogs/general.py
@@ -12,14 +12,6 @@ import config
 class General:
     def __init__(self, bot):
         self.bot = bot
-        # Remove default help command
-        self.bot.remove_command("help")
-
-    # PewDiePie Command (Hates on Pewds)
-    @commands.command()
-    async def pewdiepie(self, ctx):
-        await ctx.send("PewDiePie अद्भुत है! आपको उसकी सदस्यता लेनी चाहिए।")
-        await ctx.send("https://translate.google.com")
 
     # T-SERIES DISS TRACK
     @commands.command()
@@ -139,69 +131,6 @@ class General:
         await ctx.send(embed = em)
         print("Sent bot info. Ping time: " + str(botlat))
 
-    # Custom prefix
-    @commands.command(aliases = ["sprefix"])
-    @commands.has_permissions(manage_messages = True)
-    async def setprefix(self, ctx, *, prefix: str = None):
-        # Check if custom prefix exceeds the 30 character limit
-        if prefix != None:
-            if len(prefix) > 30:
-                em = discord.Embed(color = discord.Color.dark_teal())
-                em.add_field(name = "Prefix Character Limit Exceeded", value = "Prefixes can only be 30 characters or less")
-                await ctx.send(embed = em)
-                return
-        # Check if prefix is already in the database
-        gchck = await self.bot.pool.fetchrow("SELECT * FROM prefixes WHERE guildid = $1", ctx.guild.id)
-        # Checking and setting
-        if gchck == None:
-            if prefix != None:
-                # Insert into row
-                await self.bot.pool.execute("INSERT INTO prefixes VALUES ($1, $2)", ctx.guild.id, prefix)
-                # Tell user
-                em = discord.Embed(color = discord.Color.red())
-                em.add_field(name = "Set Prefix", value = f"{self.bot.user.mention}'s prefix has been set to `{prefix}`")
-                await ctx.send(embed = em)
-            else:
-                # Tell user
-                em = discord.Embed(color = discord.Color.dark_teal())
-                em.add_field(name = "Error: Prefix Not Set", value = "Please specify a prefix to use")
-                await ctx.send(embed = em)
-                return
-        else:
-            if prefix == None:
-                # Delete from database
-                await self.bot.pool.execute("DELETE FROM prefixes WHERE guildid = $1", ctx.guild.id)
-                # Tell user
-                em = discord.Embed(color = discord.Color.red())
-                em.add_field(name = "Prefix Removed", value = f"{self.bot.user.mention}'s prefix has been set back to the default")
-                await ctx.send(embed = em)
-            else:
-                # Update row
-                await self.bot.pool.execute("UPDATE prefixes SET prefix = $1 WHERE guildid = $2", prefix, ctx.guild.id)
-                # Tell user
-                em = discord.Embed(color = discord.Color.red())
-                em.add_field(name = "Set Prefix", value = f"{self.bot.user.mention}'s prefix has been set to `{prefix}`")
-                await ctx.send(embed = em)
-        # Update the prefix cache
-        if prefix != None:
-            self.bot.prefixes[ctx.guild.id] = prefix
-        else:
-            self.bot.prefixes.pop(ctx.guild.id)
-
-    # Returns bot prefix in the current guild
-    @commands.command(aliases = ["currentprefix", "botprefix", "serverprefix", "guildprefix"])
-    async def prefix(self, ctx):
-        # Get prefix
-        prefixes = await self.bot.pool.fetchval("SELECT prefix FROM prefixes WHERE guildid = $1", ctx.guild.id)
-        if prefixes == None:
-            prefix = "ts!, ts., t., and t!"
-        else:
-            prefix = prefixes
-        # Send
-        em = discord.Embed(color = discord.Color.red())
-        em.add_field(name = "Current Prefix", value = f"The current prefix for {self.bot.user.mention} is `{prefix}`")
-        await ctx.send(embed = em)
-
     # Invite command
     @commands.command()
     async def invite(self, ctx):
@@ -210,21 +139,6 @@ class General:
         em.add_field(name = "Invite", value = "[Invite me here!](https://discordapp.com/oauth2/authorize?client_id=500868806776979462&scope=bot&permissions=72710)")
         await ctx.send(embed = em)
         print("Sent bot invite!")
-
-    # Set prefix tutorial command
-    @commands.command(aliases = ["prefixtutorial", "tutprefix"])
-    async def prefixtut(self, ctx):
-        em = discord.Embed(color = discord.Color.dark_green())
-        em.add_field(name = "Command Use", value = f"""
-        Sets the prefix for the current server. You must have the manage messages permission to use this command.
-        **Set or change prefix**
-        `ts!setprefix [prefix here]`
-        **Revert back to default prefix**
-        `ts!setprefix`
-        **Show current prefix**
-        `ts!prefix` (does not require any special permissions to view)
-        """)
-        await ctx.send(embed = em)
 
     # Feedback command
     @commands.command()
@@ -257,64 +171,6 @@ class General:
         for b in spoiler:
             x += f"||{b}||"
         await ctx.send(x)
-
-    # Help command
-    @commands.group(invoke_without_command = True)
-    async def help(self, ctx):
-        em = discord.Embed(color = discord.Color.gold())
-        em.set_author(name = f"{self.bot.user.name} Help Page")
-        # Main commands
-        em.add_field(name = "Main Commands", value = """
-        `pewdiepie`: Tells you what I think of PewDiePie in Hindi
-        `disstrack`: Plays Bitch Lasagna in a voice channel. To disconnect, run `p.disstrack stop` or `p.disstrack leave`
-        `subcount`: Shows T-Series' and PewDiePie's subscriber count
-        `subgap`: Automatically updates the current subscriber gap between PewDiePie and T-Series every 30 seconds. Your server must be authorized to use this feature.
-        Please join the [support server](https://discord.gg/we4DQ5u) to request authorization.
-        `randomvid`: Returns a random PewDiePie or T-Series video
-        `youtube (yt)`: Sends you the link to PewDiePie's and T-Series' YouTube channel
-        `spoiler`: Sends any message you provide as a spoiler in an annoying form
-        """, inline = False)
-        # Bro Coin (economy) commands
-        em.add_field(name = "Bro Coin (economy)", value = """
-        Run `p.help economy` to view the list of commands for Bro Coin
-        """)
-        # Meta commands
-        em.add_field(name = "Meta Commands", value = f"""
-        `botinfo`: Information on {self.bot.user.name}
-        `invite`: Sends the bot invite
-        `feedback`: This command will send the developer feedback on this bot. Feel free to send suggestions or issues
-        `prefixtut`: This will give you a tutorial on how to use custom prefixes on {self.bot.user.name}
-        `prefix`: Returns the current prefix that {self.bot.user.name} uses in your server
-        """, inline = False)
-        # Timestamp
-        em.timestamp = datetime.datetime.utcnow()
-        em.set_footer(icon_url = ctx.author.avatar_url, text = f"{ctx.author.name}#{ctx.author.discriminator}")
-        await ctx.send(embed = em)
-
-    # Help: economy
-    @help.command()
-    async def economy(self, ctx):
-        em = discord.Embed(color = discord.Color.gold())
-        em.set_author(name = f"{self.bot.user.name} Economy Help Page")
-        em.add_field(name = "Bro Coin Commands", value = """
-        `shovel`: You work all day shoveling for Bro Coins
-        `balance (bal)`: Informs you on how many Bro Coins you have
-        `pay`: Pays a user with a specified amount of Bro Coins
-        `leaderboard (lb)`: Shows the leaderboard for Bro Coins
-        `gamble`: You can gamble a specific amount of Bro Coins
-        `steal (rob)`: Steals from a user that you specify
-        `transfer`: Sends Bro Coins to another server. The max amount is 50% of your coins.
-        `statistics (stats)`: Statistics on Bro Coin usage
-        """, inline = False)
-        em.add_field(name = "Shop", value = """
-        `shop`: View all the items (roles) in the shop
-        `shop add`: Adds a role to the shop (you must have the manage roles permission)
-        `shop edit`: Edits a role (eg. changes the cost) in the shop (manage roles permission required by user)
-        `shop delete (remove)`: Removes a role from the shop (manage roles permission required by user)
-        `shop buy`: Buys an item from the shop (you must have enough coins)
-        **Please note: The bot must have the manage roles permission and be higher than the role in the shop to use the shop feature**
-        """, inline = False)
-        await ctx.send(embed = em)
 
 
 def setup(bot):
