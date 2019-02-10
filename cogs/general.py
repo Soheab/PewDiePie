25483404733bfdd4c7bf7ced066a756265cdedcf
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-02-10 14:35:34 -0500

    Embed title is cleaner

--- cogs/general.py
+++ cogs/general.py
@@ -75,7 +75,7 @@ class General:
     async def botinfo(self, ctx):
         botlat = f"{self.bot.latency * 1000:.3f}"
 
-        em = discord.Embed(title = f"{self.bot.user.name} Bot Information", color = discord.Color.green())
+        em = discord.Embed(title = f"{self.bot.user.name} Information", color = discord.Color.green())
         em.add_field(name = "Bot Creator", value = "A Discord User#4063")
         em.add_field(name = "Bot Library", value = "discord.py rewrite")
         em.add_field(name = "Support Server", value = "https://discord.gg/we4DQ5u")
