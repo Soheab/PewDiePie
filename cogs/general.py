24e7b725b6f55d0f7cca915eca06a9f4edd5e572
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-02-10 14:46:41 -0500

    Removed prefixes cog and re-added the code here instead

--- cogs/general.py
+++ cogs/general.py
@@ -89,6 +89,87 @@ class General:
         em.add_field(name = "Invite", value = "[Invite me here!](https://discordapp.com/oauth2/authorize?client_id=500868806776979462&scope=bot&permissions=72710)")
         await ctx.send(embed = em)
 
+    # Set prefix tutorial command
+    @commands.command(aliases = ["prefixtutorial", "tutprefix"])
+    async def prefixtut(self, ctx):
+        em = discord.Embed(color = discord.Color.dark_green())
+        em.add_field(name = "Command Use", value = f"""
+        Sets the prefix for the current server. You must have the manage messages permission to use this command.
+        **Set or change prefix**
+        `p.setprefix [prefix here]`
+        **Revert back to default prefix**
+        `p.setprefix`
+        **Show current prefix**
+        `p.prefix` (does not require any special permissions to view)
+        """)
+        await ctx.send(embed = em)
+
+    # Returns bot prefix in the current guild
+    @commands.command(aliases = ["currentprefix", "botprefix", "serverprefix", "guildprefix"])
+    async def prefix(self, ctx):
+        prefixes = await self.bot.pool.fetchval("SELECT prefix FROM prefixes WHERE guildid = $1", ctx.guild.id)
+
+        if prefixes == None:
+            prefix = ""
+            formatted = []
+            for x in self.bot.default_prefixes:
+                formatted.append(x.lower())
+            formatted = list(dict.fromkeys(formatted))
+            for x in formatted:
+                prefix += f"{x}, "
+            if prefix.endswith(", "):
+                prefix = prefix[:-2]
+        else:
+            prefix = prefixes
+
+        em = discord.Embed(color = discord.Color.red())
+        em.add_field(name = "Current Prefix", value = f"The current prefix for {self.bot.user.mention} is `{prefix}`")
+        await ctx.send(embed = em)
+
+    # Custom prefix
+    @commands.command(aliases = ["sprefix"])
+    @commands.has_permissions(manage_messages = True)
+    async def setprefix(self, ctx, *, prefix: str = None):
+        if prefix != None:
+            if len(prefix) > 30:
+                em = discord.Embed(color = discord.Color.dark_teal())
+                em.add_field(name = "Prefix Character Limit Exceeded", value = "Prefixes can only be 30 characters or less")
+                await ctx.send(embed = em)
+                return
+
+        gchck = await self.bot.pool.fetchrow("SELECT * FROM prefixes WHERE guildid = $1", ctx.guild.id)
+
+        if gchck == None:
+            if prefix != None:
+                await self.bot.pool.execute("INSERT INTO prefixes VALUES ($1, $2)", ctx.guild.id, prefix)
+
+                em = discord.Embed(color = discord.Color.red())
+                em.add_field(name = "Set Prefix", value = f"{self.bot.user.mention}'s prefix has been set to `{prefix}`")
+                await ctx.send(embed = em)
+            else:
+                em = discord.Embed(color = discord.Color.dark_teal())
+                em.add_field(name = "Error: Prefix Not Set", value = "Please specify a prefix to use")
+                await ctx.send(embed = em)
+                return
+        else:
+            if prefix == None:
+                await self.bot.pool.execute("DELETE FROM prefixes WHERE guildid = $1", ctx.guild.id)
+
+                em = discord.Embed(color = discord.Color.red())
+                em.add_field(name = "Prefix Removed", value = f"{self.bot.user.mention}'s prefix has been set back to the default")
+                await ctx.send(embed = em)
+            else:
+                await self.bot.pool.execute("UPDATE prefixes SET prefix = $1 WHERE guildid = $2", prefix, ctx.guild.id)
+
+                em = discord.Embed(color = discord.Color.red())
+                em.add_field(name = "Set Prefix", value = f"{self.bot.user.mention}'s prefix has been set to `{prefix}`")
+                await ctx.send(embed = em)
+
+        if prefix != None:
+            self.bot.prefixes[ctx.guild.id] = prefix
+        else:
+            self.bot.prefixes.pop(ctx.guild.id)
+
     # Feedback command
     @commands.command()
     async def feedback(self, ctx, *, message: str):
