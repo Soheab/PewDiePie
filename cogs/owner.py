8481a9a3f1beefcc7e4c19203170832f50a146df
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-02-09 23:10:48 -0500

    Removed trash comments

--- cogs/owner.py
+++ cogs/owner.py
@@ -9,7 +9,7 @@ import re
 import inspect
 import sys
 sys.path.append("../")
-import config
+import config # Note: Only importing config module since it's easier to use in eval
 
 
 class Owner:
@@ -18,13 +18,9 @@ class Owner:
 
     # Command authorization check
     async def cmdauthcheck(ctx): # pylint: disable=E0213
-        # Get guild
         guild = ctx.bot.get_guild(499357399690379264)
-        # Get role
         role = guild.get_role(531176653184040961)
-        # Get member
         user = guild.get_member(ctx.author.id) # pylint: disable=E1101
-        # Check if user has role
         try:
             if role in user.roles:
                 return True
@@ -37,16 +33,15 @@ class Owner:
     @commands.command()
     @commands.check(cmdauthcheck)
     async def authorize(self, ctx):
-        # Check if guild is already in the database
         gchck = await self.bot.pool.fetchrow("SELECT * FROM authorized WHERE guildid = $1", ctx.guild.id)
         if gchck != None:
             emb = discord.Embed(color = discord.Color.dark_teal())
             emb.add_field(name = "Already Authorized", value = f"`{ctx.guild.name}` is already authorized")
             await ctx.send(embed = emb)
             return
-        # Add guild to authorization database
+
         await self.bot.pool.execute("INSERT INTO authorized VALUES ($1)", ctx.guild.id)
-        # Send message saying that it has been authorized
+
         em = discord.Embed(color = discord.Color.dark_purple())
         em.add_field(name = "Authorized", value = f"`{ctx.guild.name}` has been authorized")
         await ctx.send(embed = em)
@@ -55,16 +50,15 @@ class Owner:
     @commands.command()
     @commands.check(cmdauthcheck)
     async def deauthorize(self, ctx):
-        # Check if guild is already in the database
         gchck = await self.bot.pool.fetchrow("SELECT * FROM authorized WHERE guildid = $1", ctx.guild.id)
         if gchck == None:
             emb = discord.Embed(color = discord.Color.dark_teal())
             emb.add_field(name = "Never Authorized", value = f"`{ctx.guild.name}` was never authorized")
             await ctx.send(embed = emb)
             return
-        # Remove guild from authorization database
+
         await self.bot.pool.execute("DELETE FROM authorized WHERE guildid = $1", ctx.guild.id)
-        # Send message saying that it has been authorized
+
         em = discord.Embed(color = discord.Color.dark_purple())
         em.add_field(name = "Deauthorized", value = f"`{ctx.guild.name}` has been deauthorized")
         await ctx.send(embed = em)
