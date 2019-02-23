f62114cac1343fa36f244fae28f667160c7b3ca0
= <=>
2019-02-23 17:49:06 -0500

    Fixed indentation

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -6,7 +6,7 @@ class ErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-	@commands.Cog.listener()
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown) == False:
             try:
