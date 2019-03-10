002f1be6a801789e6bcc45a216c8b38179d83eab
Joshua Patel <23743695+joshuapatel@users.noreply.github.com>
2019-03-10 00:20:02 -0500

    Set AutoShardedBot to Bot due to critical bot issues

--- pewdiepie.py
+++ pewdiepie.py
@@ -37,7 +37,7 @@ extensions = (
 important = ("cogs.functions", "jishaku")
 
 # Bot
-class PewDiePie(commands.AutoShardedBot):
+class PewDiePie(commands.Bot):
     def __init__(self):
         self.custom_prefix = custom_prefix
         super().__init__(
