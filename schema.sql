c1171009cbcbd2bd61980b207003a84ac50456fe
Joshua Patel <test@example.com>
2019-03-16 23:45:08 -0400

    Added database schema

---
+++ schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS authorized (
+    guildid BIGINT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS econ (
+    coins BIGINT NOT NULL,
+    userid BIGINT NOT NULL,
+    guildid BIGINT NOT NULL,
+    transfer BOOL DEFAULT false NOT NULL,
+    uses INT DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS econshop (
+    roleid BIGINT NOT NULL UNIQUE,
+    guildid BIGINT NOT NULL,
+    reqamount BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS prefixes (
+    guildid BIGINT NOT NULL,
+    prefix varchar(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shovel (
+    name TEXT NOT NULL UNIQUE,
+    fate BOOL NOT NULL,
+    id INT PRIMARY KEY NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS snipe (
+    contents TEXT NOT NULL,
+    usr BIGINT NOT NULL,
+    guild BIGINT NOT NULL,
+    channel BIGINT NOT NULL,
+    message BIGINT NOT NULL UNIQUE,
+    bot BOOL NOT NULL,
+    time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS subgap (
+    msgid BIGINT NOT NULL UNIQUE,
+    channelid BIGINT NOT NULL UNIQUE,
+    guildid BIGINT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS subgapbackup (
+    msgid BIGINT NOT NULL,
+    channelid BIGINT NOT NULL,
+    guildid BIGINT NOT NULL
+);
